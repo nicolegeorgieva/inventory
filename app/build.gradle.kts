8e1c1e214779f51cad8ae1784d7726dd9abc9697
Nicole <nicole.georgievva@gmail.com>
2023-10-09 23:07:26 +0300

    Add all dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     alias(libs.plugins.devtools.ksp)
     alias(libs.plugins.dagger.hilt)
     alias(libs.plugins.kotlinx.serialization)
+    alias(libs.plugins.cashapp.molecule)
 }
 
 android {
@@ -66,16 +67,55 @@ dependencies {
     implementation(libs.kotlinx.collections.immutable)
     // endregion
 
+    // region Compose
+    implementation(libs.compose.animation)
+    implementation(libs.compose.foundation)
+    implementation(libs.compose.material3)
+    implementation(libs.compose.material3.windowsize)
+    implementation(libs.compose.runtime)
+    implementation(libs.compose.ui)
+    implementation(libs.compose.activity)
+    implementation(libs.compose.viewmodel)
+    implementation(libs.compose.tooling)
+    implementation(libs.compose.coil)
+    // endregion
+
+    // region AndroidX
+    implementation(libs.androidx.activity)
+    implementation(libs.appcompat.activity)
+    // endregion
+
     // region Hilt
     implementation(libs.hilt)
     ksp(libs.hilt.compiler)
     // endregion
 
-    // region Room
+    // region KotlinX Serialization
+    implementation(libs.kotlinx.serialization.json)
+    // endregion
+
+    // region Ktor
+    implementation(libs.ktor.client.okhttp)
+    implementation(libs.ktor.content.negotiation)
+    implementation(libs.ktor.serialization)
+    implementation(libs.ktor.client.core)
+    implementation(libs.ktor.client.serialization)
+    implementation(libs.ktor.logging)
+    // endregion
+
+    // region Local persistence (Room + DataStore)
     implementation(libs.room.runtime)
     implementation(libs.room.ktx)
     ksp(libs.room.compiler)
+    implementation(libs.datastore)
     // endregion
 
-    implementation(libs.datastore)
+    // region Testing
+    testImplementation(libs.mockk)
+    testImplementation(libs.kotest.junit5)
+    testImplementation(libs.kotest.assertions)
+    testImplementation(libs.kotest.datatest)
+    testImplementation(libs.kotlin.coroutines.test)
+    testImplementation(libs.cashapp.turbine)
+    // endregion
 }
