86570dd55942886fe1a15feb931ee160e15faa72
Nicole <nicole.georgievva@gmail.com>
2023-10-09 14:16:10 +0300

    WIP: Setup project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
-    id("org.jetbrains.kotlin.android") version "1.8.10" apply false
-    id("com.google.dagger.hilt.android") version "2.44" apply false
+    alias(libs.plugins.kotlin) apply false
+    alias(libs.plugins.dagger.hilt) apply false
 }
