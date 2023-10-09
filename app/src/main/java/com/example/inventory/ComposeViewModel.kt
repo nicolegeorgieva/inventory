79b69cdc378d392004aab73e7234c5e362d47e7e
Nicole <nicole.georgievva@gmail.com>
2023-10-09 17:08:08 +0300

    Setup ComposeViewModel

--- app/src/main/java/com/example/inventory/ComposeViewModel.kt
+++ app/src/main/java/com/example/inventory/ComposeViewModel.kt
@@ -2,15 +2,12 @@ package com.example.inventory
 
 import androidx.compose.runtime.Composable
 import androidx.lifecycle.ViewModel
-import dagger.hilt.android.lifecycle.HiltViewModel
-import javax.inject.Inject
 
 /*
 Should be used in the ViewModels of the screens.
 It's a must to override the uiState and onEvent fun - return uiState and handle events.
  */
-@HiltViewModel
-abstract class ComposeViewModel<S, E> @Inject constructor() : ViewModel() {
+abstract class ComposeViewModel<S, E> : ViewModel() {
     @Composable
     abstract fun uiState(): S
 
