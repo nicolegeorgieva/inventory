5c43fe054a8ea546f8a5219c73ecea9984e647ae
Nicole <nicole.georgievva@gmail.com>
2023-10-09 23:17:11 +0300

    Remove unnecessary gradle configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,1 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
-plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin) apply false
-    alias(libs.plugins.dagger.hilt) apply false
-    alias(libs.plugins.devtools.ksp) apply false
-    alias(libs.plugins.kotlinx.serialization) apply false
-}
