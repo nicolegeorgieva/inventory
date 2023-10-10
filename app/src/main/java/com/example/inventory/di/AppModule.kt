85b65f517ad740884f7514d45e7fffaf56a2b926
Nicole <nicole.georgievva@gmail.com>
2023-10-10 16:11:08 +0300

    Refactor AppModule

--- app/src/main/java/com/example/inventory/di/AppModule.kt
+++ app/src/main/java/com/example/inventory/di/AppModule.kt
@@ -10,11 +10,11 @@ import com.example.inventory.data.datastore.dataStore
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ActivityComponent
 import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
 
 @Module
-@InstallIn(ActivityComponent::class)
+@InstallIn(SingletonComponent::class)
 object AppModule {
     @Provides
     fun provideDataStore(
