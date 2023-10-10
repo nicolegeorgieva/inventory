56128c9837e224e2107499e43fa9569cef246f3e
Nicole <nicole.georgievva@gmail.com>
2023-10-10 14:50:30 +0300

    Implement NameRepository

---
+++ app/src/main/java/com/example/inventory/data/repository/NameRepository.kt
@@ -0,0 +1,30 @@
+package com.example.inventory.data.repository
+
+import com.example.inventory.data.datasource.NameDataSource
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+
+class NameRepository @Inject constructor(
+    private val nameDataSource: NameDataSource
+) {
+    suspend fun getName(): String? {
+        return withContext(Dispatchers.IO) {
+            nameDataSource.getName()
+        }
+    }
+
+    suspend fun setName(newName: String) {
+        if (newName.isBlank()) return
+
+        withContext(Dispatchers.IO) {
+            nameDataSource.setName(newName.trim())
+        }
+    }
+
+    suspend fun removeName() {
+        withContext(Dispatchers.IO) {
+            nameDataSource.removeName()
+        }
+    }
+}
