15f4384f2506fcf2a28ef84e462661bb11f5ea02
Nicole <nicole.georgievva@gmail.com>
2023-10-11 19:28:52 +0300

    WIP: MoreMenuScreen - support change name

--- app/src/main/java/com/example/inventory/screen/moremenu/MoreMenuViewModel.kt
+++ app/src/main/java/com/example/inventory/screen/moremenu/MoreMenuViewModel.kt
@@ -1,16 +1,34 @@
 package com.example.inventory.screen.moremenu
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
 import com.example.inventory.ComposeViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class MoreMenuViewModel @Inject constructor() : ComposeViewModel<MoreMenuState, MoreMenuEvent>() {
+    private val name = mutableStateOf("")
+
     @Composable
     override fun uiState(): MoreMenuState {
-        return MoreMenuState("")
+        return MoreMenuState(
+            name = getName()
+        )
     }
 
-    override fun onEvent(event: MoreMenuEvent) {}
+    @Composable
+    private fun getName(): String {
+        return name.value
+    }
+
+    override fun onEvent(event: MoreMenuEvent) {
+        when (event) {
+            is MoreMenuEvent.ChangeName -> setName(event.newName)
+        }
+    }
+
+    private fun setName(newName: String) {
+        name.value = newName
+    }
 }
