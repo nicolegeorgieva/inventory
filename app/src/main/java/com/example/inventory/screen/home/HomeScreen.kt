605d12e18c7e90fae2ff2b6851f93146e764e395
Nicole <nicole.georgievva@gmail.com>
2023-10-11 15:22:31 +0300

    WIP: HomeScreen

--- app/src/main/java/com/example/inventory/screen/home/HomeScreen.kt
+++ app/src/main/java/com/example/inventory/screen/home/HomeScreen.kt
@@ -1,23 +1,76 @@
 package com.example.inventory.screen.home
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MediumTopAppBar
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.inventory.ui.theme.InventoryTheme
-import kotlinx.collections.immutable.persistentListOf
 
 @Composable
 fun HomeScreen() {
     val viewModel: HomeViewModel = hiltViewModel()
     val uiState = viewModel.uiState()
+
+    HomeUi(
+        uiState = uiState,
+        onEvent = viewModel::onEvent
+    )
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeUi(
     uiState: HomeState,
     onEvent: (HomeEvent) -> Unit
 ) {
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = {
+            val scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior()
 
+            MediumTopAppBar(
+                title = {
+                    Text(
+                        "Hi",
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis
+                    )
+                },
+                actions = {
+                    IconButton(
+                        onClick = {
+                            /* doSomething() */
+                        }
+                    ) {
+                        Icon(
+                            imageVector = Icons.Filled.Menu,
+                            contentDescription = "Menu"
+                        )
+                    }
+                },
+                scrollBehavior = scrollBehavior
+            )
+        },
+        content = { innerPadding ->
+            LazyColumn(
+                contentPadding = innerPadding,
+            ) {
+
+            }
+        }
+    )
 }
 
 @Preview(showBackground = true)
@@ -25,7 +78,7 @@ fun HomeUi(
 fun HomeUiPreview() {
     InventoryTheme {
         HomeUi(
-            uiState = HomeState(inventoryList = persistentListOf()),
+            uiState = HomeState(name = null, inventoryList = null),
             onEvent = {}
         )
     }
