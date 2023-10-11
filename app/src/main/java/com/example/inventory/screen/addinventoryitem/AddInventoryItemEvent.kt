d9f12d590b9a49d9ed453977aacdf7f984c26a10
Nicole <nicole.georgievva@gmail.com>
2023-10-11 22:40:03 +0300

    WIP: AddInventoryItemViewModel; implement State and Event for the new screen

--- app/src/main/java/com/example/inventory/screen/addinventoryitem/AddInventoryItemEvent.kt
+++ app/src/main/java/com/example/inventory/screen/addinventoryitem/AddInventoryItemEvent.kt
@@ -1,3 +1,11 @@
 package com.example.inventory.screen.addinventoryitem
 
-sealed interface AddInventoryItemEvent
+sealed interface AddInventoryItemEvent {
+    data class SetName(val newName: String) : AddInventoryItemEvent
+    data class SetQuantity(val newQuantity: Int) : AddInventoryItemEvent
+    data class SetMinQuantityTarget(val newMinQuantityTarget: Int) : AddInventoryItemEvent
+    data class SetCategory(val newCategory: String) : AddInventoryItemEvent
+    data class SetDescription(val newDescription: String) : AddInventoryItemEvent
+    data class SetImage(val newImage: String) : AddInventoryItemEvent
+    data object AddInventoryItem : AddInventoryItemEvent
+}
--- app/src/main/java/com/example/inventory/screen/addinventoryitem/AddInventoryItemViewModel.kt
+++ app/src/main/java/com/example/inventory/screen/addinventoryitem/AddInventoryItemViewModel.kt
@@ -1,19 +1,123 @@
 package com.example.inventory.screen.addinventoryitem
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.viewModelScope
 import com.example.inventory.ComposeViewModel
+import com.example.inventory.data.model.InventoryItem
+import com.example.inventory.data.repository.InventoryRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import java.util.UUID
 import javax.inject.Inject
 
 @HiltViewModel
-class AddInventoryItemViewModel @Inject constructor() :
+class AddInventoryItemViewModel @Inject constructor(
+    private val inventoryRepository: InventoryRepository
+) :
     ComposeViewModel<AddInventoryItemState, AddInventoryItemEvent>() {
+
+    private val name = mutableStateOf<String?>(null)
+    private val quantity = mutableStateOf<Int?>(null)
+    private val minQuantityTarget = mutableStateOf<Int?>(null)
+    private val category = mutableStateOf<String?>(null)
+    private val description = mutableStateOf<String?>(null)
+    private val image = mutableStateOf<String?>(null)
+
     @Composable
     override fun uiState(): AddInventoryItemState {
-        TODO("Not yet implemented")
+        return AddInventoryItemState(
+            name = getName(),
+            quantity = getQuantity(),
+            minQuantityTarget = getMinQuantityTarget(),
+            category = getCategory(),
+            description = getDescription(),
+            image = getImage()
+        )
+    }
+
+    @Composable
+    private fun getName(): String? {
+        return name.value
+    }
+
+    @Composable
+    private fun getQuantity(): Int? {
+        return quantity.value
+    }
+
+    @Composable
+    private fun getMinQuantityTarget(): Int? {
+        return minQuantityTarget.value
+    }
+
+    @Composable
+    private fun getCategory(): String? {
+        return category.value
+    }
+
+    @Composable
+    private fun getDescription(): String? {
+        return description.value
+    }
+
+    @Composable
+    private fun getImage(): String? {
+        return image.value
     }
 
     override fun onEvent(event: AddInventoryItemEvent) {
-        TODO("Not yet implemented")
+        when (event) {
+            is AddInventoryItemEvent.SetCategory -> setCategory(event.newCategory)
+            is AddInventoryItemEvent.SetDescription -> setDescription(event.newDescription)
+            is AddInventoryItemEvent.SetImage -> setImage(event.newImage)
+            is AddInventoryItemEvent.SetMinQuantityTarget -> setMinQuantityTarget(
+                event.newMinQuantityTarget
+            )
+
+            is AddInventoryItemEvent.SetName -> setName(event.newName)
+            is AddInventoryItemEvent.SetQuantity -> setQuantity(event.newQuantity)
+            AddInventoryItemEvent.AddInventoryItem -> addInventoryItem()
+        }
+    }
+
+    private fun setCategory(newCategory: String) {
+        category.value = newCategory
+    }
+
+    private fun setDescription(newDescription: String) {
+        description.value = newDescription
+    }
+
+    private fun setImage(newImage: String) {
+        image.value = newImage
+    }
+
+    private fun setMinQuantityTarget(newMinQuantityTarget: Int) {
+        minQuantityTarget.value = newMinQuantityTarget
+    }
+
+    private fun setName(newName: String) {
+        name.value = newName
+    }
+
+    private fun setQuantity(newQuantity: Int) {
+        quantity.value = newQuantity
+    }
+
+    private fun addInventoryItem() {
+        viewModelScope.launch {
+            inventoryRepository.save(
+                InventoryItem(
+                    id = UUID.randomUUID(),
+                    name = getName(),
+                    quantity = getQuantity(),
+                    minQuantityTarget = getMinQuantityTarget(),
+                    category = getCategory(),
+                    description = getDescription(),
+                    imageUrl = getImage()
+                )
+            )
+        }
     }
 }
