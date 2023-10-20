f2a637c5be94cc2fe55f88949be796db19b4bdb4
Nicole <nicole.georgievva@gmail.com>
2023-10-20 15:37:47 +0300

    Put HomeScreen's InventoryItemRow and associated functions in a file in component package

--- app/src/main/java/com/example/inventory/screen/home/HomeScreen.kt
+++ app/src/main/java/com/example/inventory/screen/home/HomeScreen.kt
@@ -1,30 +1,18 @@
 package com.example.inventory.screen.home
 
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.AddCircle
-import androidx.compose.material3.Card
 import androidx.compose.material3.FabPosition
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -32,22 +20,18 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
-import coil.compose.AsyncImage
 import com.airbnb.lottie.compose.LottieAnimation
 import com.airbnb.lottie.compose.LottieCompositionSpec
 import com.airbnb.lottie.compose.rememberLottieComposition
 import com.example.inventory.R
 import com.example.inventory.screen.home.component.HomeTopAppBar
+import com.example.inventory.screen.home.component.InventoryItemRow
 import com.example.inventory.screen.home.component.SortFilterRow
 import com.example.inventory.ui.theme.InventoryTheme
 import kotlinx.collections.immutable.persistentListOf
@@ -124,142 +108,6 @@ private fun AddButton(navController: NavController?) {
         Icon(
             imageVector = Icons.Default.Add,
             contentDescription = stringResource(R.string.add)
-        )
-    }
-}
-
-@Composable
-private fun InventoryItemRow(
-    itemName: String,
-    quantity: String,
-    image: String,
-    onAddQuantity: () -> Unit,
-    onRemoveQuantity: () -> Unit
-) {
-    Card(
-        modifier = Modifier
-            .fillMaxWidth()
-            .height(64.dp)
-            .border(
-                border = BorderStroke(0.5.dp, MaterialTheme.colorScheme.onBackground),
-                shape = RoundedCornerShape(4.dp)
-            ),
-        shape = RoundedCornerShape(4.dp)
-    ) {
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Image(image = image)
-
-            ItemName(itemName = itemName)
-
-            AddRemoveQuantity(
-                onRemoveQuantity = onRemoveQuantity,
-                quantity = quantity,
-                onAddQuantity = onAddQuantity
-            )
-        }
-    }
-}
-
-@Composable
-private fun Image(
-    image: String
-) {
-    val modifier = Modifier
-        .size(64.dp)
-        .background(MaterialTheme.colorScheme.background)
-        .border(
-            border = BorderStroke(0.5.dp, MaterialTheme.colorScheme.onBackground),
-            shape = RoundedCornerShape(
-                topStart = 4.dp,
-                topEnd = 0.dp,
-                bottomStart = 4.dp,
-                bottomEnd = 0.dp
-            )
-        )
-        .padding(4.dp)
-
-    if (image.isNotBlank()) {
-        AsyncImage(
-            modifier = modifier,
-            model = image,
-            contentDescription = stringResource(R.string.item_image)
-        )
-    } else {
-        Icon(
-            modifier = modifier,
-            painter = painterResource(id = R.drawable.baseline_inventory_24),
-            contentDescription = stringResource(R.string.item_image),
-            tint = MaterialTheme.colorScheme.primary
-        )
-    }
-}
-
-@Composable
-private fun RowScope.ItemName(itemName: String) {
-    Text(
-        modifier = Modifier
-            .padding(horizontal = 8.dp)
-            .weight(1f),
-        text = itemName
-    )
-}
-
-@Composable
-private fun AddRemoveQuantity(
-    onRemoveQuantity: () -> Unit,
-    quantity: String,
-    onAddQuantity: () -> Unit
-) {
-    RemoveQuantityButton(
-        imagePainter = painterResource(id = R.drawable.baseline_remove_circle_24),
-        contentDescription = stringResource(R.string.remove),
-        onClick = onRemoveQuantity
-    )
-
-    Text(
-        modifier = Modifier.widthIn(min = 24.dp),
-        textAlign = TextAlign.Center,
-        text = quantity
-    )
-
-    AddQuantityButton(
-        imageVector = Icons.Filled.AddCircle,
-        contentDescription = stringResource(R.string.add),
-        onClick = onAddQuantity
-    )
-}
-
-@Composable
-private fun AddQuantityButton(
-    imageVector: ImageVector,
-    contentDescription: String,
-    onClick: () -> Unit
-) {
-    IconButton(
-        onClick = onClick
-    ) {
-        Icon(
-            imageVector = imageVector,
-            contentDescription = contentDescription
-        )
-    }
-}
-
-@Composable
-private fun RemoveQuantityButton(
-    imagePainter: Painter,
-    contentDescription: String,
-    onClick: () -> Unit
-) {
-    IconButton(
-        onClick = onClick
-    ) {
-        Icon(
-            painter = imagePainter,
-            contentDescription = contentDescription
         )
     }
 }
---
+++ app/src/main/java/com/example/inventory/screen/home/component/InventoryItemRow.kt
@@ -0,0 +1,183 @@
+package com.example.inventory.screen.home.component
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AddCircle
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import com.example.inventory.R
+import com.example.inventory.ui.theme.InventoryTheme
+
+@Composable
+fun InventoryItemRow(
+    itemName: String,
+    quantity: String,
+    image: String,
+    onAddQuantity: () -> Unit,
+    onRemoveQuantity: () -> Unit
+) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(64.dp)
+            .border(
+                border = BorderStroke(0.5.dp, MaterialTheme.colorScheme.onBackground),
+                shape = RoundedCornerShape(4.dp)
+            ),
+        shape = RoundedCornerShape(4.dp)
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            ItemImage(image = image)
+
+            ItemName(itemName = itemName)
+
+            AddRemoveQuantity(
+                onRemoveQuantity = onRemoveQuantity,
+                quantity = quantity,
+                onAddQuantity = onAddQuantity
+            )
+        }
+    }
+}
+
+@Composable
+private fun ItemImage(
+    image: String
+) {
+    val modifier = Modifier
+        .size(64.dp)
+        .background(MaterialTheme.colorScheme.background)
+        .border(
+            border = BorderStroke(0.5.dp, MaterialTheme.colorScheme.onBackground),
+            shape = RoundedCornerShape(
+                topStart = 4.dp,
+                topEnd = 0.dp,
+                bottomStart = 4.dp,
+                bottomEnd = 0.dp
+            )
+        )
+        .padding(4.dp)
+
+    if (image.isNotBlank()) {
+        RemoteImage(image = image, modifier = modifier)
+    } else {
+        DefaultImage(modifier = modifier)
+    }
+}
+
+@Composable
+private fun RemoteImage(
+    image: String,
+    modifier: Modifier = Modifier
+) {
+    AsyncImage(
+        modifier = modifier,
+        model = image,
+        contentDescription = stringResource(R.string.item_image)
+    )
+}
+
+@Composable
+private fun DefaultImage(modifier: Modifier = Modifier) {
+    Icon(
+        modifier = modifier,
+        painter = painterResource(id = R.drawable.baseline_inventory_24),
+        contentDescription = stringResource(R.string.item_image),
+        tint = MaterialTheme.colorScheme.primary
+    )
+}
+
+@Composable
+private fun RowScope.ItemName(itemName: String) {
+    Text(
+        modifier = Modifier
+            .padding(horizontal = 8.dp)
+            .weight(1f),
+        text = itemName
+    )
+}
+
+@Composable
+private fun AddRemoveQuantity(
+    onRemoveQuantity: () -> Unit,
+    quantity: String,
+    onAddQuantity: () -> Unit
+) {
+    RemoveQuantityButton(onClick = onRemoveQuantity)
+
+    Quantity(quantity = quantity)
+
+    AddQuantityButton(onClick = onAddQuantity)
+}
+
+@Composable
+private fun AddQuantityButton(onClick: () -> Unit) {
+    IconButton(
+        onClick = onClick
+    ) {
+        Icon(
+            imageVector = Icons.Filled.AddCircle,
+            contentDescription = stringResource(R.string.add)
+        )
+    }
+}
+
+@Composable
+private fun Quantity(quantity: String) {
+    Text(
+        modifier = Modifier.widthIn(min = 24.dp),
+        textAlign = TextAlign.Center,
+        text = quantity
+    )
+}
+
+@Composable
+private fun RemoveQuantityButton(onClick: () -> Unit) {
+    IconButton(
+        onClick = onClick
+    ) {
+        Icon(
+            painter = painterResource(id = R.drawable.baseline_remove_circle_24),
+            contentDescription = stringResource(R.string.remove)
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun InventoryItemRowPreview() {
+    InventoryTheme {
+        InventoryItemRow(
+            itemName = "Water bottles",
+            quantity = "4",
+            image = "",
+            onAddQuantity = {},
+            onRemoveQuantity = {}
+        )
+    }
+}
