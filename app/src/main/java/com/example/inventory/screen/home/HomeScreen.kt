126832822918266364778504ea8e85643f2ef578
Nicole <nicole.georgievva@gmail.com>
2023-10-20 16:02:06 +0300

    Put HomeScreen's AddButton in a file in component package

--- app/src/main/java/com/example/inventory/screen/home/HomeScreen.kt
+++ app/src/main/java/com/example/inventory/screen/home/HomeScreen.kt
@@ -5,21 +5,15 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.FabPosition
-import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
-import com.example.inventory.R
+import com.example.inventory.screen.home.component.AddButton
 import com.example.inventory.screen.home.component.EmptyInventory
 import com.example.inventory.screen.home.component.HomeTopAppBar
 import com.example.inventory.screen.home.component.InventoryItemRow
@@ -86,21 +80,6 @@ private fun HomeUi(
             }
         }
     )
-}
-
-@Composable
-private fun AddButton(navController: NavController?) {
-    FloatingActionButton(
-        onClick = {
-            navController?.navigate("addInventoryItem")
-        },
-        containerColor = MaterialTheme.colorScheme.primary
-    ) {
-        Icon(
-            imageVector = Icons.Default.Add,
-            contentDescription = stringResource(R.string.add)
-        )
-    }
 }
 
 @Preview(showBackground = true)
---
+++ app/src/main/java/com/example/inventory/screen/home/component/AddButton.kt
@@ -0,0 +1,36 @@
+package com.example.inventory.screen.home.component
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import com.example.inventory.R
+import com.example.inventory.ui.theme.InventoryTheme
+
+@Composable
+fun AddButton(navController: NavController?) {
+    FloatingActionButton(
+        onClick = {
+            navController?.navigate("addInventoryItem")
+        },
+        containerColor = MaterialTheme.colorScheme.primary
+    ) {
+        Icon(
+            imageVector = Icons.Default.Add,
+            contentDescription = stringResource(R.string.add)
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun AddButtonPreview() {
+    InventoryTheme {
+        AddButton(navController = null)
+    }
+}
