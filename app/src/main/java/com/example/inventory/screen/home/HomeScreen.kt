c69128b73df396baaca5605f370a3c28261de572
Nicole <nicole.georgievva@gmail.com>
2023-10-20 15:01:12 +0300

    Divide HomeScreen's UI logic into more functions

--- app/src/main/java/com/example/inventory/screen/home/HomeScreen.kt
+++ app/src/main/java/com/example/inventory/screen/home/HomeScreen.kt
@@ -39,6 +39,7 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -187,72 +188,100 @@ private fun SortFilterRow() {
     val ascending = remember { mutableStateOf(true) }
 
     Row(modifier = Modifier.fillMaxWidth()) {
-        Column {
-            TextButton(
-                onClick = {
-                    ascending.value = !ascending.value
-                }
-            ) {
-                Text("Sort by:")
-                if (ascending.value) {
-                    Icon(
-                        imageVector = Icons.Outlined.KeyboardArrowUp,
-                        contentDescription = "Ascending"
-                    )
-                } else {
-                    Icon(
-                        imageVector = Icons.Outlined.KeyboardArrowDown,
-                        contentDescription = "Descending"
-                    )
-                }
-            }
-        }
+        Sort(ascending = ascending)
 
         Spacer(modifier = Modifier.weight(1f))
 
-        Column {
-            val filter = remember { mutableStateOf("All") }
-            val expanded = remember { mutableStateOf(false) }
+        CategoryFilter()
+    }
+}
 
-            TextButton(
-                onClick = {
-                    expanded.value = true
-                }
-            ) {
-                Text(filter.value)
-                Icon(
-                    imageVector = Icons.Outlined.ArrowDropDown,
-                    contentDescription = "Dropdown menu"
-                )
+@Composable
+private fun Sort(
+    ascending: MutableState<Boolean>
+) {
+    Column {
+        TextButton(
+            onClick = {
+                ascending.value = !ascending.value
             }
-
-            DropdownMenu(
-                expanded = expanded.value,
-                onDismissRequest = {
-                    expanded.value = false
-                }
-            ) {
-                DropdownMenuItem(
-                    text = {
-                        Text("All")
-                    },
-                    onClick = {
-                        filter.value = "All"
-                        expanded.value = false
-                    }
+        ) {
+            Text("Sort by:")
+            if (ascending.value) {
+                Icon(
+                    imageVector = Icons.Outlined.KeyboardArrowUp,
+                    contentDescription = "Ascending"
                 )
-                DropdownMenuItem(
-                    text = {
-                        Text("Groceries")
-                    },
-                    onClick = {
-                        filter.value = "Groceries"
-                        expanded.value = false
-                    }
+            } else {
+                Icon(
+                    imageVector = Icons.Outlined.KeyboardArrowDown,
+                    contentDescription = "Descending"
                 )
             }
         }
     }
+}
+
+@Composable
+private fun CategoryFilter() {
+    Column {
+        val filter = remember { mutableStateOf("All") }
+        val expanded = remember { mutableStateOf(false) }
+
+        FilterButton(expanded = expanded, filter = filter)
+        FilterMenu(expanded = expanded, filter = filter)
+    }
+}
+
+@Composable
+private fun FilterButton(
+    expanded: MutableState<Boolean>,
+    filter: MutableState<String>
+) {
+    TextButton(
+        onClick = {
+            expanded.value = true
+        }
+    ) {
+        Text(filter.value)
+        Icon(
+            imageVector = Icons.Outlined.ArrowDropDown,
+            contentDescription = "Dropdown menu"
+        )
+    }
+}
+
+@Composable
+private fun FilterMenu(
+    expanded: MutableState<Boolean>,
+    filter: MutableState<String>
+) {
+    DropdownMenu(
+        expanded = expanded.value,
+        onDismissRequest = {
+            expanded.value = false
+        }
+    ) {
+        FilterMenuOption(name = "All", filter = filter, expanded = expanded)
+        FilterMenuOption(name = "Groceries", filter = filter, expanded = expanded)
+    }
+}
+
+@Composable
+private fun FilterMenuOption(
+    name: String,
+    filter: MutableState<String>,
+    expanded: MutableState<Boolean>
+) {
+    DropdownMenuItem(
+        text = {
+            Text(name)
+        },
+        onClick = {
+            filter.value = name
+            expanded.value = false
+        }
+    )
 }
 
 @Composable
