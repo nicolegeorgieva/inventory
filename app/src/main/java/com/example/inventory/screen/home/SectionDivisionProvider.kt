2e0ac2b91113fcdda38440c3fccd5b45f51fb7b4
Nicole <nicole.georgievva@gmail.com>
2023-10-24 14:07:24 +0300

    Refactor SectionDivisionProvider class

--- app/src/main/java/com/example/inventory/screen/home/SectionDivisionProvider.kt
+++ app/src/main/java/com/example/inventory/screen/home/SectionDivisionProvider.kt
@@ -8,28 +8,31 @@ import javax.inject.Inject
 class SectionDivisionProvider @Inject constructor() {
     fun provideItemsBySection(
         items: List<InventoryItem>,
-        minQuantityTarget: Int,
         section: SectionType
-    ): ImmutableList<InventoryItemUi> {
+    ): ImmutableList<InventoryItemUi>? {
 
         val itemsBySection = when (section) {
             SectionType.TOBUY -> items.filter {
-                it.quantity < minQuantityTarget
+                it.quantity < it.minQuantityTarget
             }
 
             SectionType.ENOUGH -> items.filter {
-                it.quantity >= minQuantityTarget
+                it.quantity >= it.minQuantityTarget
             }
         }
 
-        return itemsBySection.map {
-            InventoryItemUi(
-                id = it.id.toString(),
-                name = it.name,
-                quantity = it.quantity.toString(),
-                imagePath = it.imagePath,
-                category = it.category
-            )
-        }.toImmutableList()
+        return if (itemsBySection.isNotEmpty()) {
+            itemsBySection.map {
+                InventoryItemUi(
+                    id = it.id.toString(),
+                    name = it.name,
+                    quantity = it.quantity.toString(),
+                    imagePath = it.imagePath,
+                    category = it.category
+                )
+            }.toImmutableList()
+        } else {
+            null
+        }
     }
 }
