e7b007c186e4a5716c59ba10b9423906d929f658
Nicole <nicole.georgievva@gmail.com>
2023-10-29 00:47:13 +0300

    Add QuoteDataSource class and update AppModule

---
+++ app/src/main/java/com/example/inventory/data/datasource/QuoteDataSource.kt
@@ -0,0 +1,23 @@
+package com.example.inventory.data.datasource
+
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.request.get
+import kotlinx.serialization.Serializable
+import javax.inject.Inject
+
+class QuoteDataSource @Inject constructor(
+    private val client: HttpClient
+) {
+    @Serializable
+    data class Quotes(
+        val quotes: List<String>
+    )
+
+    suspend fun fetchQuotes(): List<String> {
+        return client.get(
+            "https://raw.githubusercontent.com/nicolegeorgieva/inventory/main/app/" +
+                    "src/main/java/com/example/inventory/quotes.json"
+        ).body<Quotes>().quotes
+    }
+}
--- app/src/main/java/com/example/inventory/di/AppModule.kt
+++ app/src/main/java/com/example/inventory/di/AppModule.kt
@@ -12,6 +12,11 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import io.ktor.client.HttpClient
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -40,5 +45,25 @@ object AppModule {
         database: AppDatabase
     ): InventoryDao {
         return database.inventoryDao()
+    }
+
+    @Provides
+    @Singleton
+    fun provideHttpClient(
+        json: Json
+    ): HttpClient {
+        return HttpClient {
+            install(ContentNegotiation) {
+                json(json)
+            }
+        }
+    }
+
+    @Provides
+    fun provideJson(): Json {
+        return Json {
+            ignoreUnknownKeys = true
+            isLenient = true
+        }
     }
 }
