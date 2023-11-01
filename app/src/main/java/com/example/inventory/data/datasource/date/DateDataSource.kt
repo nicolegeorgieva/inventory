841be457cb03ee2199bcc1be2bf56d91fc8f4a02
Nicole <nicole.georgievva@gmail.com>
2023-11-01 22:30:10 +0200

    Create DateDataSource

--- app/src/main/java/com/example/inventory/data/datasource/date/DateDataSource.kt
+++ app/src/main/java/com/example/inventory/data/datasource/date/DateDataSource.kt
@@ -2,10 +2,24 @@ package com.example.inventory.data.datasource.date
 
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import com.example.inventory.data.datastore.DATE
+import kotlinx.coroutines.flow.firstOrNull
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class DateDataSource @Inject constructor(
     private val dataStore: DataStore<Preferences>
 ) {
+    suspend fun getDate(): Long? {
+        return dataStore.data.map {
+            it[DATE]
+        }.firstOrNull()
+    }
 
+    suspend fun setDate(date: Long) {
+        dataStore.edit {
+            it[DATE] = date
+        }
+    }
 }
