f7c3c2b841af2d67aa60be772f10ce7ca751a822
Nicole <nicole.georgievva@gmail.com>
2023-11-02 18:52:12 +0200

    Update InventoryDao to use flows

--- app/src/main/java/com/example/inventory/data/database/dao/InventoryDao.kt
+++ app/src/main/java/com/example/inventory/data/database/dao/InventoryDao.kt
@@ -5,36 +5,37 @@ import androidx.room.Delete
 import androidx.room.Query
 import androidx.room.Upsert
 import com.example.inventory.data.database.entity.InventoryEntity
+import kotlinx.coroutines.flow.Flow
 import java.util.UUID
 
 @Dao
 interface InventoryDao {
     @Query("SELECT * FROM inventory_items")
-    suspend fun getAll(): List<InventoryEntity>
+    fun getAll(): Flow<List<InventoryEntity>>
 
     @Query("SELECT * FROM inventory_items ORDER BY (quantity - min_quantity_target) ASC")
-    suspend fun getAllOrderedByAscending(): List<InventoryEntity>
+    fun getAllOrderedByAscending(): Flow<List<InventoryEntity>>
 
     @Query("SELECT * FROM inventory_items ORDER BY (quantity - min_quantity_target) DESC")
-    suspend fun getAllOrderedByDescending(): List<InventoryEntity>
+    fun getAllOrderedByDescending(): Flow<List<InventoryEntity>>
 
     @Query("SELECT * FROM inventory_items WHERE category = :category")
-    suspend fun getAllByCategory(category: String): List<InventoryEntity>
+    fun getAllByCategory(category: String): Flow<List<InventoryEntity>>
 
     @Query("SELECT * FROM inventory_items WHERE id = :id")
-    suspend fun getById(id: UUID): InventoryEntity?
+    fun getById(id: UUID): Flow<InventoryEntity?>
 
     @Query(
         "SELECT * FROM inventory_items WHERE category = :category ORDER BY " +
                 "(quantity - min_quantity_target) ASC"
     )
-    suspend fun orderByAscending(category: String): List<InventoryEntity>
+    fun orderByAscending(category: String): Flow<List<InventoryEntity>>
 
     @Query(
         "SELECT * FROM inventory_items WHERE category = :category ORDER BY " +
                 "(quantity - min_quantity_target) DESC"
     )
-    suspend fun orderByDescending(category: String): List<InventoryEntity>
+    fun orderByDescending(category: String): Flow<List<InventoryEntity>>
 
     @Upsert
     suspend fun save(inventoryEntity: InventoryEntity)
