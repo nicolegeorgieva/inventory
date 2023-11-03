02c2b0110ebab0391dc4414eb065fd0c2124d05d
Nicole <nicole.georgievva@gmail.com>
2023-11-03 23:52:08 +0200

    Refactor MoreMenuViewModel

--- app/src/main/java/com/example/inventory/screen/moremenu/MoreMenuViewModel.kt
+++ app/src/main/java/com/example/inventory/screen/moremenu/MoreMenuViewModel.kt
@@ -1,11 +1,13 @@
 package com.example.inventory.screen.moremenu
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.viewModelScope
 import com.example.inventory.ComposeViewModel
 import com.example.inventory.data.repository.name.NameRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -14,8 +16,14 @@ class MoreMenuViewModel @Inject constructor(
     private val nameRepository: NameRepository
 ) : ComposeViewModel<MoreMenuState, MoreMenuEvent>() {
 
+    private val name = mutableStateOf<String?>(null)
+
     @Composable
     override fun uiState(): MoreMenuState {
+        LaunchedEffect(Unit) {
+            name.value = nameRepository.getName().first()
+        }
+
         return MoreMenuState(
             name = getName()
         )
@@ -23,7 +31,7 @@ class MoreMenuViewModel @Inject constructor(
 
     @Composable
     private fun getName(): String? {
-        return nameRepository.getName().collectAsState(initial = null).value
+        return name.value
     }
 
     override fun onEvent(event: MoreMenuEvent) {
@@ -33,6 +41,8 @@ class MoreMenuViewModel @Inject constructor(
     }
 
     private fun setName(newName: String) {
+        name.value = newName
+
         if (newName.isBlank()) {
             viewModelScope.launch {
                 nameRepository.removeName()
